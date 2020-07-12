829038c5ea77b08265ff2e9ed9687d49d2a0712f
unknown <johan.chapelain@gmail.com>
2020-07-12 21:34:59 +0200

    changes

--- coinmarketcap_ranking/main.py
+++ coinmarketcap_ranking/main.py
@@ -1,6 +1,14 @@
 from coinmarketcap_ranking.config import API_KEY
+from coinmarketcap import Market
+
+
 
 
 print ("hello world!")
 print(API_KEY)
+
+
+print("----------------retrieve data from API--------------------")
+coinmarketcap = Market()
+coinmarketcap.listings()
 
